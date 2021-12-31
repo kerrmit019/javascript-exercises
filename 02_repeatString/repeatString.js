0bdaf7e41b7bc82826e2797d66ddf56fd2e3c97f
Sarah Kerr <sarah.kerr2@hotmail.com>
2021-12-31 17:55:53 +1300

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, numberOfTimesToRepeat) {
+  let repeatedString = string;
+  //   add in an error if number is negative
+  if (numberOfTimesToRepeat < 0) {
+    return "ERROR";
+    // return empty string if number is 0 or input string is empty.
+  } else if (numberOfTimesToRepeat === 0 || string === "") {
+    return "";
+  }
 
+  for (i = 0; i < numberOfTimesToRepeat - 1; i++) {
+    repeatedString += string;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
