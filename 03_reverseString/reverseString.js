0dd734c7ac989eed2cadad325d456fe7bfff8f75
Sarah Kerr <sarah.kerr2@hotmail.com>
2021-12-31 18:16:11 +1300

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function(str) {
-
+const reverseString = function (str) {
+  //   console.log(str);
+  let reversedString = "";
+  const letters = [];
+  //   extract each letter
+  for (letter of str) {
+    letters.push(letter);
+  }
+  //   console.log(letters);
+  for (i = letters.length - 1; i >= 0; i--) {
+    // console.log(letters[i]);
+    reversedString += letters[i];
+  }
+  return reversedString;
 };
+
+// console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
