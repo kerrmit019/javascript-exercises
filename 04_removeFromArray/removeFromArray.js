cd2455f1a4b54cd52c68dbc98700318648eedbe2
Sarah Kerr <sarah.kerr2@hotmail.com>
2021-12-31 18:52:42 +1300

    Pass 1 of 7 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (inputArray, itemToBeRemoved) {
+  //    get index of item to be removed
+  let removePosition = inputArray.indexOf(itemToBeRemoved);
+  //   console.log(removePosition);
+  let firstPartNewArray = inputArray.slice(0, removePosition);
+  let secondPartNewArray = inputArray.slice(removePosition + 1);
+  let newArray = firstPartNewArray.concat(secondPartNewArray);
+  //   console.log(firstPartNewArray);
+  //   console.log(secondPartNewArray);
+  return newArray;
 };
+
+// console.log(removeFromArray(["Hello", "There", "When", "is"], "There"));
+// console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
