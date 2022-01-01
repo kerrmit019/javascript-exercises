bb8a12629cdaf8ba1c967c304f5e87e7790cc2ba
Sarah Kerr <sarah.kerr2@hotmail.com>
2022-01-01 15:18:47 +1300

    Pass 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+// summ all
+// 1, 4 = 1 + 2 + 3 + 4
+// n + 1;
+const sumAll = function (firstInt, secondInt) {
+  //   check for negative numbers
+  if (firstInt < 0 || secondInt < 0) {
+    return "ERROR";
+  }
+  //   check for non-numbers
+  if (typeof firstInt !== "number" || typeof secondInt !== "number") {
+    return "ERROR";
+  }
+  //  check which number is greater
+  let largerNum = Math.max(firstInt, secondInt);
+  let smallerNum = Math.min(firstInt, secondInt);
 
+  let sum = 0;
+
+  for (i = smallerNum; i <= largerNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
+// console.log(sumAll(1, 4));
 // Do not edit below this line
 module.exports = sumAll;
