598250b0288594cd804c3cd775d8eda0082a5a73
Sarah Kerr <sarah.kerr2@hotmail.com>
2022-01-30 12:59:24 +1300

    Complete findTheOldest - SK version

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const oldest = people.sort((a, b) => {
+    if (a.yearOfDeath === undefined) {
+      aDeath = new Date().getFullYear();
+    } else {
+      aDeath = a.yearOfDeath;
+    }
 
+    if (b.yearOfDeath === undefined) {
+      bDeath = new Date().getFullYear();
+    } else {
+      bDeath = b.yearOfDeath;
+    }
+
+    // console.log({ aDeath }, { bDeath });
+
+    return aDeath - a.yearOfBirth > bDeath - b.yearOfBirth ? -1 : 1;
+  });
+  //   console.log(oldest);
+  return oldest[0];
 };
 
 // Do not edit below this line
