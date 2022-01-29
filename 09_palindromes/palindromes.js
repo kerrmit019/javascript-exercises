5aaace04fc67b3077b11232fd24043d2ab741257
Sarah Kerr <sarah.kerr2@hotmail.com>
2022-01-29 22:41:52 +1300

    Complete test with one word no punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+  let sentenceReversed = "";
 
+  console.log(sentence.split(""));
+  for (let i = sentence.split("").length - 1; i >= 0; i--) {
+    console.log(sentence[i]);
+    sentenceReversed += sentence[i];
+  }
+
+  console.log(sentenceReversed);
+
+  return sentence === sentenceReversed ? true : false;
 };
 
+// palindromes("radar");
 // Do not edit below this line
 module.exports = palindromes;
