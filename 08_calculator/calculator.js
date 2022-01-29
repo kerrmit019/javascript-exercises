6db3c639d128d99cf4eb98ea5160ddbbc94285a1
Sarah Kerr <sarah.kerr2@hotmail.com>
2022-01-29 22:19:40 +1300

    Complete calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (x) {
+  return x.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (x) {
+  return x.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  if (x === 0) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
 };
 
-// Do not edit below this line
+// // Do not edit below this line
 module.exports = {
   add,
   subtract,
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
