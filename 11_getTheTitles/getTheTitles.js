932df972cc9bf648242a572a149aa205fc266381
Sarah Kerr <sarah.kerr2@hotmail.com>
2022-01-30 11:47:58 +1300

    Complete getTheTitles - SK version

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  const bookTitles = books.map((book) => {
+    return book.title;
+  });
+  return bookTitles;
 };
 
-// Do not edit below this line
+// const books = [
+//   {
+//     title: "Book",
+//     author: "Name",
+//   },
+//   {
+//     title: "Book2",
+//     author: "Name2",
+//   },
+// ];
+
+// getTheTitles(books);
+// // Do not edit below this line
 module.exports = getTheTitles;
