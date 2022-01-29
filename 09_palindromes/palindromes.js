f5a1e96c8eac1689115d3f9a9663b73e328c59e9
Sarah Kerr <sarah.kerr2@hotmail.com>
2022-01-29 23:06:08 +1300

    Complete palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,12 @@
 const palindromes = function (sentence) {
+  //clean up sentence of punctuation, and make lowercase
+  sentence = sentence.replace(/\W/g, "").toLowerCase();
+
   let sentenceReversed = "";
 
-  console.log(sentence.split(""));
+  //   console.log(sentence.split(""));
   for (let i = sentence.split("").length - 1; i >= 0; i--) {
-    console.log(sentence[i]);
+    // console.log(sentence[i]);
     sentenceReversed += sentence[i];
   }
 
@@ -12,6 +15,6 @@ const palindromes = function (sentence) {
   return sentence === sentenceReversed ? true : false;
 };
 
-// palindromes("radar");
+// palindromes("radar!");
 // Do not edit below this line
 module.exports = palindromes;
